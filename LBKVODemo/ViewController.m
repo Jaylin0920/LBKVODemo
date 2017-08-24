0d8e35a2848ad9880cdbf8a754579d5c509b84c1
JiBaoBao <yemanlanshan@sina.com>
2017-08-24 14:50:42 +0800

    add system kvo

--- LBKVODemo/ViewController.m
+++ LBKVODemo/ViewController.m
@@ -9,6 +9,8 @@
 #import "ViewController.h"
 #import "NSObject+LBKVO.h"
 
+#define kKeyPath @"backgroundColor"
+
 @interface ViewController ()
 
 @property (weak, nonatomic) IBOutlet UILabel *testLbael;
@@ -18,25 +20,51 @@
 @implementation ViewController
 
 - (void)dealloc{
-    [self.view lb_removeObserver:self.testLbael key:@"backgroundColor"];
+    [self.view removeObserver:self forKeyPath:@"backgroundColor" context:nil];
+    [self.view lb_removeObserver:self.testLbael key:kKeyPath];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //方法1 系统kvo
+//    [self useKVO_system];
+    
+    //方法2 自定义kvo(blcok形式)
+    [self useKVO_LB];
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    self.view.backgroundColor = [UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:1.0];
+    self.testLbael.text = @"123";
+}
+
+
+#pragma mark - system KVO
+
+- (void)useKVO_system{
+    [self.view addObserver:self forKeyPath:@"backgroundColor" options:NSKeyValueObservingOptionNew context:nil];
+}
+
+// 系统KVO 接收方法
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context{
+    NSLog(@"useKVO_system");
+}
+
+
+#pragma mark - LB KVO
+
+- (void)useKVO_LB{
     // textLabel要监听colorView的backgroundColor属性
-    [self.view lb_addObserver:self.testLbael key:@"backgroundColor" callback:^(UILabel *observer, NSString *key, id oldValue, id newValue) {
+    [self.view lb_addObserver:self.testLbael key:kKeyPath callback:^(UILabel *observer, NSString *key, id oldValue, id newValue) {
+            NSLog(@"useKVO_LB");
         // 回到主线程刷新UI界面
         dispatch_async(dispatch_get_main_queue(), ^{
-            NSLog(@"颜色改变了!");
             observer.text = [NSString stringWithFormat:@"currentBGColor = %@", newValue];
         });
     }];
 }
 
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    self.view.backgroundColor = [UIColor colorWithRed:arc4random_uniform(255)/255.0 green:arc4random_uniform(255)/255.0 blue:arc4random_uniform(255)/255.0 alpha:1.0];
-}
 
 
 @end
